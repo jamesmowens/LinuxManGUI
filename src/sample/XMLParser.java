ca42019065d1bdf5fd664526a25e3ff04724b776
James Owens <jamesmarshallowens@gmail.com>
2017-04-12 23:53:13 -0400

    Added xml parsing stuff

--- src/sample/XMLParser.java
+++ src/sample/XMLParser.java
@@ -9,5 +9,31 @@ import java.io.*;
  */
 public class XMLParser {
 
-    
+    Document doc;
+
+    public XMLParser(){
+
+        try {
+            doc = this.makeDocBuuilder().parse(new File("manpages.xml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private DocumentBuilder makeDocBuuilder(){
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+
 }
