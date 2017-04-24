9c3815a725b978e8c890762da682e4ac09366ac1
James Owens <jamesmarshallowens@gmail.com>
2017-04-24 07:47:56 -0400

    Created data singleton

--- src/model/DataSingleton.java
+++ src/model/DataSingleton.java
@@ -4,4 +4,14 @@ package model;
  * Created by James on 4/24/2017.
  */
 public class DataSingleton {
+
+    static DataSingleton data = new DataSingleton();
+
+    private DataSingleton(){};
+
+    public static DataSingleton getInstance(){
+        return data;
+    }
+
+
 }
