faa8585a3bd55779d925a4c02c2f0bcef1cd1a74
James Owens <jamesmarshallowens@gmail.com>
2017-04-25 03:00:36 -0400

    Removed test methods

--- src/model/DataSingleton.java
+++ src/model/DataSingleton.java
@@ -45,28 +45,11 @@ public class DataSingleton {
 
 
 
-   /* public Section getExampleSection(){
-        return new Section("User Commands",getExamplePages());
-    }*/
 
     public String getCurrentSectionName() {
        return currentSection.getName();
     }
 
-   /* public ArrayList<Page> getExamplePages(){
-        Page p1 = new Page("Page 1",new HashMap<>(),new ArrayList<>());
-        Page p2 = new Page("Page 2",new HashMap<>(),new ArrayList<>());
-        Page p3 = new Page("Page 3",new HashMap<>(),new ArrayList<>());
-        Page p4 = new Page("Page 4",new HashMap<>(),new ArrayList<>());
-
-        ArrayList<Page> pages =  new ArrayList<>();
-        pages.add(p1);
-        pages.add(p2);
-        pages.add(p3);
-        pages.add(p4);
-
-        return pages;
-    }*/
 
     public Page getCurrentPage(){
         return currentPage;
